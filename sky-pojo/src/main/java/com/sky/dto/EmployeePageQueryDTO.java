b01dfb0b7e5475773d69e7481879d10271deea6e
hellolazy <1185373083@qq.com>
2026-03-09 16:15:31 +0800

    新增了员工的分页查询操作，包含时间格式化

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,19 +1,23 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("员工分页查询提供的数据模型")
 public class EmployeePageQueryDTO implements Serializable {
 
-    //员工姓名
+    @ApiParam("员工姓名")
     private String name;
 
-    //页码
+    @ApiParam("页码")
     private int page;
 
-    //每页显示记录数
+    @ApiParam("每页显示记录数")
     private int pageSize;
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,9 +3,11 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.dto.PasswordEditDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -117,4 +119,20 @@ public class EmployeeController {
         employeeService.addEmployee(employeeDTO);
         return Result.success();
     }
+
+    /**
+     * 分页查询员工信息
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "分页查询员工信息")
+    public Result<PageResult> pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("分页查询员工信息，页码：{}，页大小：{}，员工姓名：{}", employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize(), employeePageQueryDTO.getName());
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+
 }
